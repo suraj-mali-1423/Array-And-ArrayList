5d3b774f5e35cada43743a88fb81252cf1e85763
Suraj Mali <surajsmali@coep.sveri.ac.in>
2025-02-27 00:38:09 +0530

    260. Single Number III
    Given an integer array nums, in which exactly two elements appear only once and all the other elements appear exactly twice. Find the two elements that appear only once. You can return the answer in any order.
    
    You must write an algorithm that runs in linear runtime complexity and uses only constant extra space.
    
    Example 1:
    
    Input: nums = [1,2,1,3,2,5]
    Output: [3,5]
    Explanation:  [5, 3] is also a valid answer.
    Example 2:
    
    Input: nums = [-1,0]
    Output: [-1,0]
    Example 3:
    
    Input: nums = [0,1]
    Output: [1,0]

---
+++ SingleNumberIII.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+public class SingleNumberIII {
+    public static int [] SingleIII( int [] arr ){
+           int n = arr.length;
+           int [] helper = new int[2];
+           Arrays.sort(arr);
+           int j = 0 ;
+        for (int i = 0; i < n-1; i +=2) {
+                if ( arr[i] != arr[i+1]){
+                    helper[j] = arr[i];
+                    j++;
+                }
+        }
+
+        if (arr[n-1] != arr[n-2]){
+            helper[j] = arr[n-1];
+            j++;
+        }
+
+        return  helper;
+    }
+    public static void main(String[] args) {
+          int [] arr  = { 1,2,1,3,2,5};
+          int [] result = SingleIII(arr);
+          for( int i = 0 ; i < result.length ; i++ ){
+              System.out.print(result[i] +"  ");
+          }
+    }
+}
